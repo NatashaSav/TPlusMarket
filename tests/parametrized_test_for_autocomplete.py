7bb6e9b37b0b67cfd8074603d2d7502041fd3a67
natalia.osnovenko <natalia.osnovenko@arixess.com>
2023-04-12 01:48:19 +0300

    added changed testing_autocomplete.py

--- tests/parametrized_test_for_autocomplete.py
+++ tests/parametrized_test_for_autocomplete.py
@@ -31,32 +31,9 @@ class TestParametrizedAutocomplete:
             assert expected_response in item, f"expected to see {search_phrase} in actual phrase {item}"
 
     @staticmethod
-    @pytest.mark.parametrize("search_phrase, expected_items_count", [('fhgjskdnckvj', 0),
-                                                                     ('abrakadabra', 0),
-                                                                     ('lalala', 0),
-                                                                     ('alloha', 0),
-                                                                     ('wkdfj', 0),
-                                                                     ('+980348', 0),
-                                                                     ('-45678', 0),
-                                                                     ('+364', 0),
-                                                                     (' ', 0)])
-    def test_incorrect_and_empty_data(before_all_fixture, search_phrase, expected_items_count, base_url):
-        items_data = BasePage.get_all_items_from_response(base_url, INNER_URL, search_phrase, before_all_fixture)
-        assert len(items_data) == expected_items_count, f"expected items count is {expected_items_count}, " \
-                                                        f"actual items count is {items_data}"
-
-    @staticmethod
     @pytest.mark.parametrize("search_phrase, expected_status_code", [('bluetooth', 401),
                                                                      ('Автомобільний освіжувач повітря', 401),
-                                                                     ('Baseus Fabric Artifact Car Fragrance', 401),
-                                                                     ('Чохол Cover Samsung Galaxy', 401),
-                                                                     ('Чохол Cover Samsung Galaxy S10', 401),
-                                                                     ('Samsung Galaxy red', 401),
-                                                                     ('Захисне скло', 401),
-                                                                     ('Apple Watch 38mm', 401),
-                                                                     ('iPhone 14 Plus', 401),
-                                                                     ('iPhone 12', 401),
-                                                                     ('iPhone 10', 401)])
+                                                                     ('Baseus Fabric Artifact Car Fragrance', 401)])
     def test_check_status_code_of_unauthorized_user(search_phrase, expected_status_code, base_url):
         my_url = f"{base_url}/{INNER_URL}"
         params = {'search': search_phrase}
@@ -66,19 +43,39 @@ class TestParametrizedAutocomplete:
                                                   f"actual status code is {response.status_code}"
 
     @staticmethod
-    @pytest.mark.parametrize("test_input, expected_response", [("!@#", 200),
-                                                               ("*&%", 200),
-                                                               ("(&^)", 200),
-                                                               ("+!#", 200),
-                                                               ("{@#}", 200),
-                                                               ("$#|&", 200),
-                                                               ("_)&(", 200),
-                                                               ("$%&*", 200)])
-    def test_special_symbols(before_all_fixture, test_input, expected_response, base_url):
+    @pytest.mark.parametrize("test_input, expected_response, expected_items_count",
+                             [("!@#", 200, 0),
+                              ("*&%", 200, 0),
+                              ("$%&*", 200, 0),
+                              ('fhgjskdnckvj', 200, 0),
+                              ('Wk0Dfj14', 200, 0),
+                              ('-45678', 200, 0),
+                              ('+364', 200, 0),
+                              (' ', 200, 0)])
+    def test_special_symbols(before_all_fixture, test_input, expected_response, expected_items_count, base_url):
         my_url = f"{base_url}/{INNER_URL}"
         params = {'search': test_input}
         response = before_all_fixture.get(url=my_url, params=params)
-        assert response.status_code == expected_response
+        items_data = response.json()["items"]
+        assert (len(items_data) == expected_items_count) and response.status_code == expected_response,\
+               f"expected items count is {expected_items_count}, " \
+               f"actual items count is {items_data}"
+
+    @staticmethod
+    @pytest.mark.parametrize("test_input, expected_response, expected_items_count",
+                             [("SELECT * FROM users WHERE username = 'john' OR 'a'='a';-- AND password = ''", 200, 0),
+                              ("SELECT * FROM db_table WHERE stud_name = %B;", 200, 0),
+                              ("SELECT * FROM users WHERE id=1 ; DELETE FROM users;", 200, 0),
+                              ("1234 ' AND 1=0 UNION ALL SELECT 'admin', '81dc9bdb52d04dc20036dbd8313ed055", 200, 0)])
+    def test_sql_injections(before_all_fixture, test_input, expected_response, expected_items_count, base_url):
+        my_url = f"{base_url}/{INNER_URL}"
+        params = {'search': test_input}
+        response = before_all_fixture.get(url=my_url, params=params)
+        items_data = response.json()["items"]
+        assert (len(items_data) == expected_items_count) and response.status_code == expected_response, \
+               f"expected items count is {expected_items_count}, " \
+               f"actual items count is {items_data}"
+
 
 
 
